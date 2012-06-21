0110f1d98197f601acc15d688e22cfd006b2cf3d
Sammy1Am <greeneggsandsamuel@gmail.com>
2012-06-20 18:56:37 -0700

    Added objectSerialization for preferences Switched to using MoppyControlWindow

--- Java/MoppyDesk/src/moppydesk/MoppyUI.java
+++ Java/MoppyDesk/src/moppydesk/MoppyUI.java
@@ -4,6 +4,15 @@
  */
 package moppydesk;
 
+import gnu.io.NoSuchPortException;
+import gnu.io.PortInUseException;
+import gnu.io.UnsupportedCommOperationException;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+import javax.sound.midi.MidiUnavailableException;
+import moppydesk.ui.MoppyControlWindow;
 import org.jdesktop.application.SingleFrameApplication;
 
 /**
@@ -12,11 +21,14 @@ import org.jdesktop.application.SingleFrameApplication;
  */
 public class MoppyUI extends SingleFrameApplication{
 
-    MoppySequencer ms = null;
+    public MoppySequencer ms = null;
+    
+    public Preferences prefs = Preferences.userNodeForPackage(MoppyUI.class);
     
     @Override
     protected void startup() {
-        MoppyMainWindow mainWindow = new MoppyMainWindow(this);
+        //MoppyMainWindow mainWindow = new MoppyMainWindow(this);
+        MoppyControlWindow mainWindow = new MoppyControlWindow(this);
         show(mainWindow);
     }
     
@@ -26,5 +38,74 @@ public class MoppyUI extends SingleFrameApplication{
     public static void main(String[] args) {
         launch(MoppyUI.class, args);
     }
+    
+    public MoppySequencer initializeSequencer() {
+        try {
+            if (ms!= null){
+                ms.closeSequencer();
+                ms = null;
+            }
+
+            ms = new MoppySequencer(prefs.get(Constants.PREF_COM_PORT,null));
+            
+        } catch (NoSuchPortException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (PortInUseException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedCommOperationException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MidiUnavailableException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return ms;
+    }
+    
+    public void shutdownSequencer(){
+        if (ms != null){
+            ms.closeSequencer();
+            ms = null;
+        }
+    }
+
+    public void putPreferenceObject(String key, Object object){
+        try {
+            prefs.putByteArray(key, serializePref(object));
+        } catch (IOException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Object getPreferenceObject(String key){
+        try {
+            return deserializePref(prefs.getByteArray(key, null));
+        } catch (NullPointerException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MoppyUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+    }
+    
+//Preference to object
+  private static Object deserializePref(byte[] b) throws NullPointerException, IOException, ClassNotFoundException
+  {
+    return new ObjectInputStream(new ByteArrayInputStream(b)).readObject();
+  }
+  
+//Object to preference
+  private static byte[] serializePref(Object p) throws IOException
+  {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(baos);
+    oos.writeObject(p);
+    oos.close();
+    return baos.toByteArray();
+  }
     
 }
